aed74770850c7a398f8463115c924e2b54ceab14
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2022-03-18 22:49:56 +0300

    chore: ignore style for lib folder

--- lib/nodes.d.ts
+++ lib/nodes.d.ts
@@ -1,31 +1,26 @@
 import { Nil, Nilable } from "./lang.js";
 export declare type AnyNode = TextNode | Elem | Frag | Nil | false;
-
-export declare class TextNode extends String {}
-
+export declare class TextNode extends String {
+}
 export declare function F(...children: AnyNode[]): Frag;
 export declare class Frag {
-  #private;
-  constructor();
-  get children(): Nilable<AnyNode[]>;
-  withText(text: string): this;
-  addText(text: string): void;
-  withChildren(...nodes: AnyNode[]): this;
-  addChild(node: AnyNode): void;
+    #private;
+    constructor();
+    get children(): Nilable<AnyNode[]>;
+    withText(text: string): this;
+    addText(text: string): void;
+    withChildren(...nodes: AnyNode[]): this;
+    addChild(node: AnyNode): void;
 }
-export declare function E(
-  tagName: string,
-  attrs: ElemAttrs,
-  ...children: AnyNode[]
-): Elem;
+export declare function E(tagName: string, attrs: ElemAttrs, ...children: AnyNode[]): Elem;
 export declare type ElemAttrs = Record<string, unknown>;
 export declare class Elem extends Frag {
-  #private;
-  constructor(tagName: string);
-  get tagName(): string;
-  get attrs(): Record<string, unknown>;
-  withAttrs(attrs: Record<string, unknown>): Elem;
-  withAttr(name: string, value: unknown): Elem;
-  addAttr(name: string, value: unknown): void;
-  addChild(node: AnyNode): void;
+    #private;
+    constructor(tagName: string);
+    get tagName(): string;
+    get attrs(): Record<string, unknown>;
+    withAttrs(attrs: Record<string, unknown>): Elem;
+    withAttr(name: string, value: unknown): Elem;
+    addAttr(name: string, value: unknown): void;
+    addChild(node: AnyNode): void;
 }
