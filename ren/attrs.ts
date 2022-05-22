f1d6a2e98934a4c10afe86558d5d43084722eaa4
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2022-05-22 12:47:24 +0300

    allow single string in class names attribute

--- ren/attrs.ts
+++ ren/attrs.ts
@@ -1,7 +1,7 @@
 import { Attrs } from "../core/node.ts";
-import { isNotSkip, join, Skipable } from "../core/utils.ts";
+import { isNotSkip, isStr, join, Skipable } from "../core/utils.ts";
 
-export function classNames(vals: Skipable<string>[]): Attrs {
-  const val = join(" ", vals.filter(isNotSkip));
+export function classNames(vals: string | Skipable<string>[]): Attrs {
+  const val = isStr(vals) ? vals : join(" ", vals.filter(isNotSkip));
   return !val.length ? {} : { class: val };
 }
